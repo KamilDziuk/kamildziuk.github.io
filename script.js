ae8383cfebfb7f735959e55b5849768aaf8d34b8
kamildziuk <kamil.dziuk@o2.pl>
2023-06-18 22:45:22 +0200

    Changing the background color, adding links to GitHub and Linkedin. Code optimization.

--- script.js
+++ script.js
@@ -16,3 +16,14 @@ $(document).on('click', 'a', function(event){
     }, 1250); 
     $('html, body').animate({path : new $.path.bezier(bezier_params)}); 
 });
+document.addEventListener('DOMContentLoaded', function() {
+    var socialIcons = document.getElementById('socialIcons');
+    socialIcons.addEventListener('click', function(event) {
+      var target = event.target;
+      if (target.tagName === 'I') {
+        var parentLink = target.parentNode;
+        var href = parentLink.getAttribute('href');
+        window.open(href, '_blank');
+      }
+    });
+  });
