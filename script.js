7e69e2399499ea729649e50d422189c08a93c873
kamildziuk <kamil.dziuk@o2.pl>
2023-06-20 14:40:01 +0200

    Code optimization in CSS, JS and HTML. Improved text margins.

--- script.js
+++ script.js
@@ -1,30 +1,29 @@
 new WOW().init();
-$(window).scroll(function() {
+$(window).scroll(function () {
     var scroll = $(window).scrollTop();
 
     if (scroll >= 50) {
-        $(".fixed-top").addClass("change-fixed-top"); 
+        $(".fixed-top").addClass("change-fixed-top");
     } else {
-        $(".fixed-top").removeClass("change-fixed-top"); 
+        $(".fixed-top").removeClass("change-fixed-top");
     }
 });
-$(document).on('click', 'a', function(event){
-    event.preventDefault(); 
+$(document).on('click', 'a', function (event) {
+    event.preventDefault();
 
     $('html, body').animate({
-        scrollTop: $( $.attr(this, 'href') ).offset().top 
-    }, 1250); 
-    $('html, body').animate({path : new $.path.bezier(bezier_params)}); 
+        scrollTop: $($.attr(this, 'href')).offset().top
+    }, 1250);
+    $('html, body').animate({ path: new $.path.bezier(bezier_params) });
 });
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function () {
     var socialIcons = document.getElementById('socialIcons');
-    socialIcons.addEventListener('click', function(event) {
-      var target = event.target;
-      if (target.tagName === 'I') {
-        var parentLink = target.parentNode;
-        var href = parentLink.getAttribute('href');
-        window.open(href, '_blank');
-      }
+    socialIcons.addEventListener('click', function (event) {
+        var target = event.target;
+        if (target.tagName === 'I') {
+            var parentLink = target.parentNode;
+            var href = parentLink.getAttribute('href');
+            window.open(href, '_blank');
+        }
     });
-  });
-  
+});
