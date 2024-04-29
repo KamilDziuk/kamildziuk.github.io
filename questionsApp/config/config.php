f965711d8a42ed8b391f1455b0d15a58c4fcd97c
kamildziuk <kontakt@kamildziuk.pl>
2024-04-29 08:26:36 +0200

    Adding questionsApp folder

---
+++ questionsApp/config/config.php
@@ -0,0 +1,12 @@
+<!-- Connecting to the database (SQL) start -->
+<?php
+$config = array(
+'servername' => 'mariadb1011.server171344.nazwa.pl',
+'username' => 'server171344_questionsApp',
+'password' => '1wA$Eq&HAhL#L&wlJo#7s-#26w8*H&s1u#?L5awrlp!I&Hlmiw2dr14hOK',
+'dbname' => 'server171344_questionsApp'
+);
+
+
+?>
+<!-- Connecting to the database (SQL) end -->
