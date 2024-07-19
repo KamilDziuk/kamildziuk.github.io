fe7cc266efab7279658a1197e1deb912f9004238
kamildziuk <kontakt@kamildziuk.pl>
2024-07-19 17:54:08 +0200

    Method for changing the language in the GitHubRESTAPI section (gitApi.js)

--- js/gitApi.js
+++ js/gitApi.js
@@ -13,9 +13,21 @@ let data = await url.json();
  data.forEach(element => {
   textElement += `<a href="${element.html_url}"><p>${element.name}</p></a>`;
  });
-
  showRepo.innerHTML = textElement;
- lengthRepo.innerHTML =  `<p>My Repository List<br><br>Total number: ${data.length}</p>`;
+
  textElement += "</ul>";
+ let polishLanguage =  document.querySelector('.polishLanguage');
+ lengthRepo.innerHTML =  `<p>My repository List<br><br>Total number: ${data.length}</p>`;
+  
+    polishLanguage.addEventListener('click', () =>{
+  if(polishLanguage.textContent == 'English'){
+    lengthRepo.innerHTML =  `<p>Moje repozytoria<br><br>Łączna liczba: ${data.length}</p>`;
+  }
+   else if(polishLanguage.textContent == 'Polish'){
+        lengthRepo.innerHTML =  `<p>My repository List<br><br>Total number: ${data.length}</p>`;
+        }
+ 
+})
+
 };
 gitAPI();
