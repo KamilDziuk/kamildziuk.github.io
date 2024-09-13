17580bb5ef48e4944752dbca93c294a92d69a735
kamildziuk <kontakt@kamildziuk.pl>
2024-09-13 08:58:46 +0200

    1: Improve all slider reaction

--- js/slider.js
+++ js/slider.js
@@ -1,7 +1,4 @@
 let galleryItem = document.querySelectorAll('.portfolioImage');
-
-let numbers = 0;
-
 function  gallery(index)
 {
     galleryItem.forEach((item, i) => {
@@ -11,45 +8,77 @@ function  gallery(index)
 
     
 };
+let numbers = 0;
+let moveGallery;
+goSlider = () => {
+ moveGallery = setInterval( () =>
+    {
+        numbers = (numbers < galleryItem.length -3) ?  numbers +1:0;
+        gallery(numbers);
+    
+    },4000);
+    
+}
+goSlider();
+galleryItem.forEach(element => {
+    element.style.transition = "transform 3s ease";
+});
+
+galleryItem.forEach(element2 => {
+    element2.addEventListener("mouseleave", ()=> {
+       
+        goSlider();
+     })
+});
+normalMove = () => 
+
+    {
+        galleryItem.forEach(element1 => {
+            element1.style.transition = "transform 3s ease";
+        });
+    }
 
 
 
 
 
 
-setInterval( () =>
-{
-    numbers = (numbers < galleryItem.length -3) ?  numbers +1:0;
-    gallery(numbers);
-
-},6000)
 
 
+
+
+galleryItem.forEach(element => {
+    
+
+    element.addEventListener("mousemove", ()=> {
+        
+   clearTimeout(moveGallery);
+})
+});
 document.querySelector('.top').addEventListener('click', () => {
 numbers = (numbers < galleryItem.length - 1)? numbers + 1: 0;
+normalMove();
 gallery(numbers);
+clearTimeout(moveGallery);
 });
 
 document.querySelector('.buttom').addEventListener('click', () => {
 numbers = (numbers > 0) ? numbers -1: galleryItem.length -1;
+normalMove();
+clearTimeout(moveGallery);
 gallery(numbers);
 });
 
+document.querySelector('.top').addEventListener('mouseleave', () => {
+    goSlider();
+});
+
+
+document.querySelector('.buttom').addEventListener('mouseleave', () => {
+    goSlider();
+});
+
 gallery(numbers);
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
@@ -68,14 +97,30 @@ function  propertyText(index)
 
     
 };
-
-setInterval( () =>
+let moveGalleryBar;
+goSliderBar = () => {
+ moveGalleryBar = setInterval( () =>
 {
     propertyNumbers = (propertyNumbers < property.length -1) ?  propertyNumbers +1:0;
     propertyText(propertyNumbers);
 
 },5000)
+}
+goSliderBar();
+property.forEach(element2 => {
+    
 
+    element2.addEventListener("mousemove", ()=> {
+        
+   clearTimeout(moveGalleryBar);
+})
+})
 
+property.forEach(element3 => {
+    
 
-propertyText(propertyNumbers);
+    element3.addEventListener("mouseleave", ()=> {
+        
+        goSliderBar();
+})
+})
