923c0ac89a53a5d0f4b0025d82831c39862bb98e
kamildziuk <kamildziukwebdeveloper@gmail.com>
2025-09-02 22:32:34 +0200

    fix(menu): Fixed behavior relative to scrolling
    fixed behavior of styles when scrolling the page

--- src/components/Hyperlinks/useNavigationIndicator.ts
+++ src/components/Hyperlinks/useNavigationIndicator.ts
@@ -9,8 +9,8 @@ export default function useNavigationIndicator() {
   >([]);
   useEffect(() => {
     function navigationIndicatorStatus() {
-      const windowHeightPositionValues = [0, 300, 1900, 2700];
-      const windowHeightPositionValuesHide = [300, 1900, 2700, 4000];
+      const windowHeightPositionValues = [0, 300, 2800, 3500];
+ const windowHeightPositionValuesHide = [300, 2800, 3500, 4000];
       const windowHeightPosition = window.scrollY;
       setNavigationIndicatorOn(
         windowHeightPositionValues.map((whpv) => whpv < windowHeightPosition)
